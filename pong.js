794b38602b0253ce9de1758a8baa35580554790c
phuong <khanh-phuong.NGUYEN@etu.univ-amu.fr>
2023-02-02 20:44:27 +0100

    pc

--- pong.js
+++ pong.js
@@ -1,50 +1,47 @@
-document.addEventListener("DOMContentLoaded" ,function();){
-    launch();
-}
+"use strict";
+document.addEventListener('DOMContentLoaded', init);
+
+let id;
+let x;
+let y;
+let vx;
+let vy;
 
 function Ball() {
     this.id = "ball";
-    this.x = 0;
-    this.y = 0;
-    this.vx = 0;
-    this.vy = 0;
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
 }
 function place_objects(objects) {
-    for(let object of objects) {
+    for (let object of objects) {
         let element = document.getElementById(object.id);
         element.style.left = object.x + "px";
         element.style.top = object.y + "px";
     }
+    ball.x = x;
+    ball.y = y;
+    ball.vx = 5;
+    ball.vy = 5;
 }
+
 function update() {
-    ball.x += 5;
-    ball.y += 5;
+    if(ball.x <= 0|| ball.x > document.body.getBoundingClientRect().width +64){
+        ball.vx = -ball.vx;
+    }
+    if(ball.y <= 0|| ball.y > document.body.getBoundingClientRect().height +64){
+        ball.vy = -ball.vy;
+    }
+
+    ball.x += ball.vx;
+    ball.y += ball.vy;
     place_objects([ball]);
 }
 let ball;
+
 function init() {
     ball = new Ball();
     setInterval(update, 100);
 }
-
-function track_player_input(event) {
-    if(event.type == "keydown") {
-        switch(event.key) {
-            case "a": buttons.p1_up = true; break;
-            case "q": buttons.p1_down = true; break;
-            case "p": buttons.p2_up = true; break;
-            case "m": buttons.p2_down = true; break;
-        }
-    } 
-    else if(event.type == "keyup") {
-        switch(event.key) {
-        case "a": buttons.p1_up = false; break;
-        case "q": buttons.p1_down = false; break;
-        case "p": buttons.p2_up = false; break;
-        case "m": buttons.p2_down = false; break;
-        }
-    }
-}
-document.addEventListener("keydown", track_player_input);
-document.addEventListener("keyup", track_player_input);
-document.body.getBoundingClientRect();
+init();
