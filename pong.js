ebef9b147aa8d96f7b97881d8cd8cb9053d9c34f
phuong <khanh-phuong.NGUYEN@etu.univ-amu.fr>
2023-02-04 15:18:48 +0100

    "qua la ok"

--- pong.js
+++ pong.js
@@ -1,10 +1,35 @@
+"use strict";
 document.addEventListener('DOMContentLoaded', init);
 
 let id;
-let x = 200;
-let y = 200;
-let vx = 5;
-let vy = 5;
+let x = window.innerWidth /2;
+let y = window.innerHeight /2;
+let vx = 5*randomV();
+let vy = 5*randomV();
+
+let ball;
+let paddleL;
+let paddleR;
+let scoreL = 0;
+let scoreR = 0;
+
+let ballElement = document.getElementById("ball");
+let ballWidth = ballElement.width;
+let ballHeight = ballElement.height;
+let paddleHeight = document.getElementById("pad1").height;
+let paddleWidth = document.getElementById("pad1").width;
+
+function random(max){
+    return Math.floor(Math.random() * max);
+}
+
+function randomV(){
+    let a = random(2);
+    if(a === 0){
+        return -1
+    }
+    return 1
+}
 
 function Ball() {
     this.id = "ball";
@@ -29,9 +54,19 @@ function place_objects(objects) {
 }
 
 function update() {
+    
     if (ball.x <= 0 || ball.x > document.body.getBoundingClientRect().width - 64) {
+        
+        if(ball.x <= 0){
+            scoreR++;
+        } else {
+            scoreL++;
+        }
+
         ball.vx = -ball.vx;
+        document.getElementById("score").innerHTML= `${scoreL}-${scoreR}`;
     }
+
     if (ball.y <= 0 || ball.y > document.body.getBoundingClientRect().height - 64) {
         ball.vy = -ball.vy;
     }
@@ -50,40 +85,71 @@ function update() {
     let pad2 = document.getElementById("pad2");
     let pad2Rect = pad2.getBoundingClientRect();
 
-    if (ball.x <= pad2Rect.right &&
-        ball.x >= pad2Rect.left &&
+    if (ball.x + ballWidth <= pad2Rect.right &&
+        ball.x + ballWidth >= pad2Rect.left &&
         ball.y >= pad2Rect.top &&
         ball.y <= pad2Rect.bottom) {
-
         ball.vx = -ball.vx;
     }
 
     ball.x += ball.vx;
     ball.y += ball.vy;
+
     place_objects([ball, paddleL, paddleR]);
 }
 
-let ball;
-let paddleL;
-let paddleR;
+function reset(){
+    
+    scoreL = 0;
+    scoreR = 0;
+    document.getElementById("score").innerHTML= `${scoreL}-${scoreR}`;
+
+}
 
 function init() {
+    
     ball = new Ball();
-    paddleL = new Paddle("pad1", 10, 200);
-    paddleR = new Paddle("pad2", window.innerWidth - 40, 200);
+    paddleL = new Paddle("pad1", 0, 200);
+    paddleR = new Paddle("pad2", window.innerWidth - 20, 200);
 
     document.addEventListener("keydown", function (event) {
-        if (event.code === "KeyQ") {
-            paddleL.y -= 20;
-        } else if (event.code === "KeyA") {
-            paddleL.y += 20;
-        } else if (event.code === "KeyP") {
-            paddleR.y -= 20;
-        } else if (event.code === "KeyL") {
-            paddleR.y += 20;
-        }
+        
+        switch(event.code) {
+
+            case "KeyQ":
+                if(paddleL.y>0){
+                    paddleL.y -= 20;
+                }
+                break;
+
+            case "KeyA":
+                if(paddleL.y<window.innerHeight-paddleHeight){
+                    paddleL.y += 20;
+                }
+                break;
+
+            case "KeyP":
+                if(paddleR.y>0){
+                    paddleR.y -= 20;
+                }
+                break;
+
+            case "KeyL":
+                if(paddleR.y<window.innerHeight-paddleHeight){
+                    paddleR.y += 20;
+                }
+                break;
+          }
+    
         place_objects([paddleL, paddleR]);
     });
 
-    setInterval(update, 100);
+    setInterval(update, 10);
 }
+
+
+
+
+
+
+
